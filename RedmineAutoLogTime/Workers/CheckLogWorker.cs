0b1619d363e5b83451169a1ae76258d1304c65ba
anh.phamviet <anh.phamviet@vti.com.vn>
2024-05-31 13:26:15 +0700

    Add logger to CheckLogWorker and refactor error handling
    The logger has been added to the CheckLogWorker class to handle and log exceptions better. A refactoring was carried out to provide better error handling for operations that might throw exceptions, such as checking log time and adding log time to issues, improving overall stability and fault-tolerance.

--- RedmineAutoLogTime/Workers/CheckLogWorker.cs
+++ RedmineAutoLogTime/Workers/CheckLogWorker.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using RedmineAutoLogTime.Enums;
 using RedmineAutoLogTime.Interfaces.Services;
 using RedmineAutoLogTime.Models;
@@ -15,43 +16,64 @@ public class CheckLogWorker : BackgroundService
 {
     private readonly IConfiguration _configuration;
     private readonly IRedmineService _redmineService;
+    private readonly ILogger<CheckLogWorker> _logger;
 
-    public CheckLogWorker(IConfiguration configuration, IRedmineService redmineService)
+    public CheckLogWorker(IConfiguration configuration, IRedmineService redmineService, ILogger<CheckLogWorker> logger)
     {
         _configuration = configuration;
         _redmineService = redmineService;
+        _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            var logTimes = await _redmineService.GetTodayLogTimesAsync();
-            var total = logTimes.Sum(l => l.Hours);
-            // if total > 8 then message box 
-            if (total > 8)
+            try
             {
-                // Show message box 
-                MessageBox.Show("You have logged more than 8 hours today!");
+                await CheckLogTimeAsync(stoppingToken);
             }
-
-            if (total == 0)
+            catch (Exception e)
             {
-                var activity = _configuration.GetValue<int>("activity");
-                var comment = _configuration.GetValue<string>("comment");
-                var issueId = _configuration.GetValue<string>("issueId");
+                _logger.LogError(e, "An error occurred in CheckLogTimeAsync");
+            }
+        }
+    }
+
+    private async Task CheckLogTimeAsync(CancellationToken stoppingToken)
+    {
+        var logTimes = await _redmineService.GetTodayLogTimesAsync();
+        var total = logTimes.Sum(l => l.Hours);
+        // if total > 8 then message box 
+        if (total > 8)
+        {
+            // Show message box 
+            MessageBox.Show("You have logged more than 8 hours today!");
+        }
+
+        if (total == 0)
+        {
+            var activity = _configuration.GetValue<int>("activity");
+            var comment = _configuration.GetValue<string>("comment");
+            var issueId = _configuration.GetValue<string>("issueId");
                 
-                // add validate for configuration
-                if (issueId != null && !string.IsNullOrEmpty(comment))
+            // add validate for configuration
+            if (issueId != null && !string.IsNullOrEmpty(comment))
+            {
+                try 
                 {
                     await _redmineService.AddLogTimeToIssueAsync(issueId, 8, comment,
                         DateTime.Now.ToString("yyyy-MM-dd"), (RedmineActivity)activity);
                     MessageBox.Show("A logtime has been created!");
+                } 
+                catch(Exception e) 
+                {
+                    _logger.LogError(e, "An error occurred while adding logtime");
                 }
             }
-
-            const int delayMinutes = 10;
-            await Task.Delay(TimeSpan.FromMinutes(delayMinutes), stoppingToken);
         }
+
+        const int delayMinutes = 10;
+        await Task.Delay(TimeSpan.FromMinutes(delayMinutes), stoppingToken);
     }
 }
