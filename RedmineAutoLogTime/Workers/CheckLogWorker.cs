b9f43488d8868fbf06b0ca6a0294c2564f6d3e2a
anh.phamviet <anh.phamviet@vti.com.vn>
2024-06-04 16:21:57 +0700

    Refactor CheckLogWorker class constructor
    The CheckLogWorker class constructor has been refactored to improve code readability and eliminate the use of private read-only field declarations. The required services (IConfiguration, IRedmineService, ILogger) are now directly accessed in the method bodies, instead of being assigned to private fields in the constructor.

--- RedmineAutoLogTime/Workers/CheckLogWorker.cs
+++ RedmineAutoLogTime/Workers/CheckLogWorker.cs
@@ -6,25 +6,17 @@ using System.Windows;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using RedmineAutoLogTime.Enums;
 using RedmineAutoLogTime.Interfaces.Services;
 using RedmineAutoLogTime.Models;
 
 namespace RedmineAutoLogTime.Workers;
 
-public class CheckLogWorker : BackgroundService
+public class CheckLogWorker(
+    IConfiguration configuration,
+    IRedmineService redmineService,
+    ILogger<CheckLogWorker> logger)
+    : BackgroundService
 {
-    private readonly IConfiguration _configuration;
-    private readonly IRedmineService _redmineService;
-    private readonly ILogger<CheckLogWorker> _logger;
-
-    public CheckLogWorker(IConfiguration configuration, IRedmineService redmineService, ILogger<CheckLogWorker> logger)
-    {
-        _configuration = configuration;
-        _redmineService = redmineService;
-        _logger = logger;
-    }
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
@@ -35,14 +27,14 @@ public class CheckLogWorker : BackgroundService
             }
             catch (Exception e)
             {
-                _logger.LogError(e, "An error occurred in CheckLogTimeAsync");
+                logger.LogError(e, "An error occurred in CheckLogTimeAsync");
             }
         }
     }
 
     private async Task CheckLogTimeAsync(CancellationToken stoppingToken)
     {
-        var logTimes = await _redmineService.GetTodayLogTimesAsync();
+        var logTimes = await redmineService.GetTodayLogTimesAsync();
         var total = logTimes.Sum(l => l.Hours);
         switch (total)
         {
@@ -53,22 +45,22 @@ public class CheckLogWorker : BackgroundService
                 break;
             case 0:
             {
-                var activity = _configuration.GetSection("activity").Get<Activity>();
-                var comment = _configuration.GetValue<string>("comment");
-                var issueId = _configuration.GetValue<string>("issueId");
+                var activity = configuration.GetSection("activity").Get<Activity>();
+                var comment = configuration.GetValue<string>("comment");
+                var issueId = configuration.GetValue<string>("issueId");
                 
                 // add validate for configuration
                 if (issueId != null && !string.IsNullOrEmpty(comment) && activity != null)
                 {
                     try 
                     {
-                        await _redmineService.AddLogTimeToIssueAsync(issueId, 8, comment,
+                        await redmineService.AddLogTimeToIssueAsync(issueId, 8, comment,
                             DateTime.Now.ToString("yyyy-MM-dd"), activity);
                         MessageBox.Show("A logtime has been created!");
                     } 
                     catch(Exception e) 
                     {
-                        _logger.LogError(e, "An error occurred while adding logtime");
+                        logger.LogError(e, "An error occurred while adding logtime");
                     }
                 }
 
